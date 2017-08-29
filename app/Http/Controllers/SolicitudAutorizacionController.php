149af682294dd9013e50753702ee70533c4d4880
FRANCO JESUS MAMANI POZO <mamanipozofrancojesus@gmail.com>
2017-08-29 18:06:02 -0400

    SolicitudAutorizacion Migration Controller Model

--- app/Http/Controllers/SolicitudAutorizacionController.php
+++ app/Http/Controllers/SolicitudAutorizacionController.php
@@ -7,79 +7,28 @@ use Illuminate\Http\Request;
 
 class SolicitudAutorizacionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\SolicitudAutorizacion  $solicitudAutorizacion
-     * @return \Illuminate\Http\Response
-     */
     public function show(SolicitudAutorizacion $solicitudAutorizacion)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\SolicitudAutorizacion  $solicitudAutorizacion
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(SolicitudAutorizacion $solicitudAutorizacion)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\SolicitudAutorizacion  $solicitudAutorizacion
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, SolicitudAutorizacion $solicitudAutorizacion)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\SolicitudAutorizacion  $solicitudAutorizacion
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(SolicitudAutorizacion $solicitudAutorizacion)
     {
-        //
+
     }
 }
