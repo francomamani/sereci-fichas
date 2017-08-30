0f5da5aa0412298a0461f55efd0be4dc34b885d7
FRANCO JESUS MAMANI POZO <mamanipozofrancojesus@gmail.com>
2017-08-30 15:12:54 -0400

    Descomentado en api.php

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
 Route::post('autenticar', 'UserController@autenticar');
 Route::group(['middleware'=>['jwt-auth']], function (){
     Route::get('usuario', 'UserController@usuario');
