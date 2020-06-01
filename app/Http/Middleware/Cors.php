1abaa9245d438fb0adc83b8b3b2bb3eecfebcd2a
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2020-06-01 12:34:16 -0400

    actualizacion de cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,11 +9,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, X-XSRF-TOKEN');
-
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
         return $response;
     }
 }
